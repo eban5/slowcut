c8f5c6c13095d65b9a802f3799c76cb74d31bdde
Esteban Amas <estebanamas@gmail.com>
2021-02-02 20:59:36 -0500

    Update structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,32 @@
 import React from "react";
+import { CustomNavbar as Navbar } from "./Navbar";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 	return (
 		<div className="App">
+			{/* Navbar */}
+			<Navbar />
 			<header className="App-header">
-				{/* Navbar */}
-
 				{/* Showcase / Hero */}
-
-				{/* Carousel - Featured Movies */}
-
-				{/* Popular */}
-
-				{/* About - Features / Links */}
-
-				{/* Popular Reviews This Week */}
-
-				{/* Popular Lists */}
-
-				{/* Recent News */}
-
-				{/* Footer */}
 
 				<h1>Slowcut</h1>
 				<p>A letterboxd clone.</p>
 			</header>
+			{/* Carousel - Featured Movies */}
+
+			{/* Popular */}
+
+			{/* About - Features / Links */}
+
+			{/* Popular Reviews This Week */}
+
+			{/* Popular Lists */}
+
+			{/* Recent News */}
+
+			{/* Footer */}
 		</div>
 	);
 }
