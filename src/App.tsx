c250808a1f9ad4d7690e5338c5a6704139a8fdc0
Esteban Amas <estebanamas@gmail.com>
2021-02-02 20:39:42 -0500

    Scaffolding for client

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				{/* Navbar */}
+
+				{/* Showcase / Hero */}
+
+				{/* Carousel - Featured Movies */}
+
+				{/* Popular */}
+
+				{/* About - Features / Links */}
+
+				{/* Popular Reviews This Week */}
+
+				{/* Popular Lists */}
+
+				{/* Recent News */}
+
+				{/* Footer */}
+
+				<h1>Slowcut</h1>
+				<p>A letterboxd clone.</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
