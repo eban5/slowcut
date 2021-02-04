c83f05dffc3c5cafc1418b883cdb718f17482ea8
Esteban Amas <estebanamas@gmail.com>
2021-02-03 21:23:43 -0500

    Carousel of movie posters

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,64 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-	const OMDB_API_KEY: string = process.env.REACT_APP_OMDB_API_KEY || "";
+	// const OMDB_API_KEY: string = process.env.REACT_APP_OMDB_API_KEY || "";
 
 	const [slides, setSlides] = useState([]);
 
 	useEffect(() => {
-		const testURL: any = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=batman`;
+		// const testURL: any = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=batman`;
 
-		fetch(testURL)
-			.then((success) => {
-				console.log(success.json())
-			})
-			.then((movies) => {
-				console.log(movies);
-			})
-			.catch((error) => console.error(error));
-		// console.log("collection", collection);
-		// setSlides(collection);
+		// fetch(testURL)
+		
 	}, []);
 
 	return (
-		<div className="App">
+		<>
+			{/* Background image */}
+			<div className="background_container">
+				<div className="background-wrapper">
+
+				<div className="background-image"></div>
+				<div className="background-mask"></div>
+				</div>
+			</div>
+
 			{/* Navbar */}
-			<Navbar />
+			<div className="container_navbar">
+				<Navbar />
+			</div>
 
-			{/* Showcase / Hero */}
-			<Showcase />
+			{/* Content */}
+			<div className="App">
+				<div className="content-wrap">
+					{/* Showcase / Hero */}
+					<div id="showcase">
+						<Showcase />
+					</div>
 
-			{/* Carousel - Featured Movies */}
-			<Carousel />
+					{/* Carousel - Featured Movies */}
+					<div id="carousel">
+						<Carousel />
+					</div>
 
-			{/* Popular */}
+					{/* Popular */}
+					<section id="popular">Popular</section>
 
-			{/* About - Features / Links */}
+					{/* About - Features / Links */}
+					<section id="about">About</section>
 
-			{/* Popular Reviews This Week */}
+					{/* Popular Reviews This Week */}
+					<section id="popular-reviews">Popular Reviews</section>
 
-			{/* Popular Lists */}
+					{/* Popular Lists */}
+					<section id="popular-lists">Popular Lists</section>
 
-			{/* Recent News */}
-
-			{/* Footer */}
-		</div>
+					{/* Recent News */}
+					<section id="recent-news">Recent News</section>
+				</div>
+			</div>
+			<div className="footer">{/* Footer */}</div>
+		</>
 	);
 }
 
--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -1,45 +1,30 @@
-import React from "react";
-import { Carousel } from "react-bootstrap";
-// import { Splide, SplideSlide } from '@splidejs/react-splide';
+import React, { useEffect, useState } from "react";
+import "./Carousel.css";
+import axios from "axios";
 
 export const CustomCarousel = () => {
+	const OMDB_API_KEY: string = process.env.REACT_APP_OMDB_API_KEY || "";
+
+	const [posters, setPosters] = useState<any>([]);
+
+	useEffect(() => {
+		const testURL: any = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=batman`;
+
+		axios
+			.get(testURL)
+			.then((response) => {
+        setPosters(response.data.Search);
+			})
+			.catch((error) => console.error(error));
+	}, []);
+
 	return (
-		<Carousel>
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src="holder.js/800x400?text=First slide&bg=373940"
-					alt="First slide"
-				/>
-				<Carousel.Caption>
-					<h3>First slide label</h3>
-					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src="holder.js/800x400?text=Second slide&bg=282c34"
-					alt="Third slide"
-				/>
-
-				<Carousel.Caption>
-					<h3>Second slide label</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src="holder.js/800x400?text=Third slide&bg=20232a"
-					alt="Third slide"
-				/>
-
-				<Carousel.Caption>
-					<h3>Third slide label</h3>
-					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-		</Carousel>
+		<ul className="carousel_content">
+			{posters.map((item: any, index: number) => {
+				return (<li key={index}>
+					<img className="poster" src={item.Poster}></img>
+				</li>);
+			})}
+		</ul>
 	);
 };
