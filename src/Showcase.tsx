150ce06ca961b87032063f43e0bd97624bc8b37a
Esteban Amas <estebanamas@gmail.com>
2021-02-03 21:24:05 -0500

    Spacing overall and layout, Showcase done

--- src/Showcase.tsx
+++ src/Showcase.tsx
@@ -1,12 +1,27 @@
 import "./Showcase.css";
-import "./App.css"
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 export const Showcase = () => {
 	return (
 		<div className="showcase_container">
-			<div className="showcase_content vertical-center">
-				{/* <img alt="showcase"/> */}
-				<h1>Slowcut</h1>
+			<div className="showcase_content">
+				<div className="showcase_title">
+					<h2>
+						Track films you’ve watched. Save those you want to see. Tell your friends
+						what’s good.{" "}
+					</h2>
+				</div>
+				<div className="showcase_button">
+					<Button>Get Started - It's Free</Button>
+				</div>
+				<div className="showcase_subtitle">
+					{" "}
+					The social network for film lovers. Also available on iOS, Apple TV
+					<span className="showcase_brand_icon--apple"></span>
+					<span className="showcase_brand_icon--android"></span>
+				</div>
+
 				<p>A letterboxd clone.</p>
 			</div>
 		</div>
