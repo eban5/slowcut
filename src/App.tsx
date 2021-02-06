707922de21f7b9b767a1af04073b353f87a2b6cb
Esteban Amas <estebanamas@gmail.com>
2021-02-06 16:04:18 -0500

    Brand route back to home

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,7 @@ import slowcut from "./images/slowcut.png";
 
 // react-bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-	Navbar,
-	Button,
-	Form,
-	FormControl,
-	Nav,
-	NavDropdown
-} from "react-bootstrap";
+import { Navbar, Button, Form, FormControl, Nav, NavDropdown } from "react-bootstrap";
 import MovieDetail from "./components/MovieDetail";
 import LandingPage from "./components/LandingPage";
 
@@ -23,62 +16,59 @@ const NoMatchRoute = () => <div>404 Page</div>;
 function App() {
 	return (
 		<>
-			
-				{/* Navbar */}
-				<div className="container_navbar">
-					{/* <Navbar /> */}
+			{/* Navbar */}
+			<div className="container_navbar">
+				{/* <Navbar /> */}
 
-					<Navbar className="nav" variant="dark" expand="lg">
+				<Navbar className="nav" variant="dark" expand="lg">
+					<Link to="/">
 						<Navbar.Brand href="#home">
 							<span className="nav_icon">
 								<img src={slowcut} alt="slowcut icon" />
 							</span>
 							<span className="nav_brand">Slowcut</span>
 						</Navbar.Brand>
+					</Link>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="mr-auto">
+							{/* <Link to="/"> */}
+							<Nav.Link className="nav_link" href="#home">
+								Films
+							</Nav.Link>
+							{/* </Link> */}
+							{/* <Link to="/"> */}
+							<Nav.Link className="nav_link" href="#link">
+								Lists
+							</Nav.Link>
+							{/* </Link> */}
+							<NavDropdown
+								title="Members"
+								className="nav_link"
+								id="basic-nav-dropdown">
+								<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.2">
+									Another action
+								</NavDropdown.Item>
+								<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item href="#action/3.4">
+									Separated link
+								</NavDropdown.Item>
+							</NavDropdown>
+						</Nav>
+						<Form inline>
+							<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+							<Button variant="outline-success">Search</Button>
+						</Form>
+					</Navbar.Collapse>
+				</Navbar>
 
-						<Navbar.Toggle aria-controls="basic-navbar-nav" />
-						<Navbar.Collapse id="basic-navbar-nav">
-							<Nav className="mr-auto">
-								{/* <Link to="/"> */}
-								<Nav.Link className="nav_link" href="#home">
-									Films
-								</Nav.Link>
-								{/* </Link> */}
-								{/* <Link to="/"> */}
-								<Nav.Link className="nav_link" href="#link">
-									Lists
-								</Nav.Link>
-								{/* </Link> */}
-								<NavDropdown
-									title="Members"
-									className="nav_link"
-									id="basic-nav-dropdown">
-									<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-									<NavDropdown.Item href="#action/3.2">
-										Another action
-									</NavDropdown.Item>
-									<NavDropdown.Item href="#action/3.3">
-										Something
-									</NavDropdown.Item>
-									<NavDropdown.Divider />
-									<NavDropdown.Item href="#action/3.4">
-										Separated link
-									</NavDropdown.Item>
-								</NavDropdown>
-							</Nav>
-							<Form inline>
-								<FormControl type="text" placeholder="Search" className="mr-sm-2" />
-								<Button variant="outline-success">Search</Button>
-							</Form>
-						</Navbar.Collapse>
-					</Navbar>
-
-					<Switch>
-						<Route exact path="/" component={LandingPage} />
-						<Route path="/movie/:imdbID" component={MovieDetail} />
-					</Switch>
-				</div>
-
+				<Switch>
+					<Route exact path="/" component={LandingPage} />
+					<Route path="/movie/:imdbID" component={MovieDetail} />
+				</Switch>
+			</div>
 		</>
 	);
 }
